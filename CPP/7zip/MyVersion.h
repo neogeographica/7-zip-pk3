66b65ed4f03e9966f0b69f311e20431139d0cdaa
Joel Baxter <joel.baxter@neogeographica.com>
2013-11-10 15:21:34 -0800

    version change for upcoming minor release

--- CPP/7zip/MyVersion.h
+++ CPP/7zip/MyVersion.h
@@ -1,9 +1,9 @@
 #define MY_VER_MAJOR 9
 #define MY_VER_MINOR 20
 #define MY_VER_BUILD 0
-#define MY_VER_PK3_BUILD 1
-#define MY_VERSION "9.20-pk3-1"
-#define MY_7ZIP_VERSION "7-Zip 9.20-pk3-1"
+#define MY_VER_PK3_BUILD 2
+#define MY_VERSION "9.20-pk3-1.1"
+#define MY_7ZIP_VERSION "7-Zip 9.20-pk3-1.1"
 #define MY_DATE "2010-11-18"
 #define MY_COPYRIGHT "Copyright (c) 1999-2010 Igor Pavlov"
 #define MY_VERSION_COPYRIGHT_DATE MY_VERSION "  " MY_COPYRIGHT "  " MY_DATE
